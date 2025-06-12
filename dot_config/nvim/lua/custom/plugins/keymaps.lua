ed21fd79ebbe98128d474aa986aa8cd7b99f7744
George Louie <george.k.louie@gmail.com>
2025-06-12 00:28:20 -0700

    update yazi and update keymap, removed dups, add description and make it more accessible to me

--- dot_config/nvim/lua/custom/plugins/keymaps.lua
+++ dot_config/nvim/lua/custom/plugins/keymaps.lua
@@ -9,26 +9,34 @@ vim.g.mapleader = ' '
 -- Disables the spacebar key from default behavior in Normal and Visual mode
 vim.keymap.set({ 'n', 'v' }, '<Space>', '<Nop>', { silent = true })
 
+
+-- which key directory, starting with <leader>
+require('which-key').register {
+    ['<leader>f'] = { name = '[F]ind with telescope', _ = 'which_key_ignore' },
+    ['<leader>l'] = { name = '[L]ua', _ = 'which_key_ignore' },
+    ['<leader>p'] = { name = '[P]ython', _ = 'which_key_ignore' },
+}
+
 -- lua keymaps
-vim.keymap.set('n', '<leader><leader>x', '<cmd>source %<CR>', { desc = 'Source current file.' })
-vim.keymap.set('n', '<leader>e', ':.lua<CR>', { desc = 'Execute lua line.' })
-vim.keymap.set('n', '<leader>e', ':lua<CR>', { desc = 'Execute highlighted lua lines.' })
+vim.keymap.set('n', '<leader>ls', '<cmd>:so ~/.config/nvim/init.lua<CR>', { desc = '[S]ource nvim init file.' })
+vim.keymap.set('n', '<leader>lf', '<cmd>source %<CR>', { desc = 'Source current [f]ile.' })
+vim.keymap.set('n', '<leader>le', ':.lua<CR>', { desc = '[E]xecute lua line.' })
+vim.keymap.set('v', '<leader>le', ':lua<CR>', { desc = '[E]xecute highlighted lua lines.' })
+vim.keymap.set({ 'n', 'v' }, '<space>lr', ':!lua %<CR>', { silent = true, desc = '[R]un lua on entire file.' })
 
 -- python keymaps
-vim.keymap.set('n', '<leader>py', ':!python3 %<CR>', { desc = 'Run python3 on this file.' })
+vim.keymap.set('n', '<leader>pr', ':!python3 %<CR>', { desc = '[R]un python3 on this file.' })
 
 -- Remap for dealing with word wrap
 vim.keymap.set('n', 'k', "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
 vim.keymap.set('n', 'j', "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
 
-vim.keymap.set('n', '<leader>pv', '<cmd>:Vex<CR>') -- Opens netrw (file explorer)
-vim.keymap.set('n', '<leader><CR>', '<cmd>:so ~/.config/nvim/init.lua<CR>')
-
-vim.keymap.set('n', '<C-p>', '<cmd>:FZF<CR>')
+-- fzf
+vim.keymap.set('n', '<C-p>', '<cmd>:FZF<CR>', { desc = 'Open FZF file finder' })
 
 -- easy split generation
-vim.keymap.set('n', '<leader>|', ':vsplit<CR>') -- space+| creates vertical split
-vim.keymap.set('n', '<leader>-', ':split<CR>')  -- space+- creates horizontal split
+vim.keymap.set('n', '<leader>|', ':vsplit<CR>', { desc = 'Create vertical split' })
+vim.keymap.set('n', '<leader>-', ':split<CR>', { desc = 'Create horizontal split' })
 
 -- -- easy split navigation
 -- Keybinds to make split navigation easier.
@@ -39,20 +47,23 @@ vim.keymap.set('n', '<C-h>', '<C-w><C-h>', { desc = 'Move focus to the left wind
 vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right window' })
 vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
 vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
+
+-- buffer navigation
+vim.keymap.set('n', '<Tab>', ':bnext <CR>', { desc = 'Goes to next buffer(file)' })
+vim.keymap.set('n', '<S-Tab>', ':bprevious <CR>', { desc = 'Goes to previous buffer(file)' })
+vim.keymap.set('n', '<leader>x', ':bd <CR>', { desc = 'Closes current buffer without saving' })
+vim.keymap.set('n', '<leader>b', '<cmd> enew <CR>', { desc = 'Creates a new buffer' })
 --
-vim.keymap.set('n', '<Tab>', ':bnext <CR>')                  -- goes to next buffer(file)
-vim.keymap.set('n', '<S-Tab>', ':bprevious <CR>')            -- goes to previous buffer
-vim.keymap.set('n', '<leader>x', ':bd <CR>')                 -- delete the current buffer
-vim.keymap.set('n', '<leader>b', '<cmd> enew <CR>')          -- new buffer
---
-vim.keymap.set('n', '<C-S-Left>', ':vertical resize +3<CR>') -- resize vertical by 3
-vim.keymap.set('n', '<C-S-Right>', ':vertical resize -3<CR>')
+
+-- pane split navigation
+vim.keymap.set('n', '<leader>tK', '<C-w>t<C-w>K', { desc = 'Make pane split vertically' })
+vim.keymap.set('n', '<leader>tH', '<C-w>t<C-w>H', { desc = 'Make pane split horizontally' })
 
 -- vertical scrop and center
-vim.keymap.set('n', '<C-d>', '<C-d>zz')
-vim.keymap.set('n', '<C-u>', '<C-u>zz')
+vim.keymap.set('n', '<C-d>', '<C-d>zz', { desc = 'Scroll down and center' })
+vim.keymap.set('n', '<C-u>', '<C-u>zz', { desc = 'Scroll up and center' })
 
--- Find and center
+-- Find and center, to be used with the find command
 vim.keymap.set('n', 'n', 'nzzzv', opts)
 vim.keymap.set('n', 'N', 'Nzzzv', opts)
 
@@ -67,38 +78,39 @@ vim.keymap.set('i', 'kj', '<ESC>')
 vim.keymap.set('i', 'jk', '<ESC>')
 
 -- Tabs
-vim.keymap.set('n', '<leader>to', ':tabnew<CR>', opts)
-vim.keymap.set('n', '<leader>tx', ':tabcloase<CR>', opts)
-vim.keymap.set('n', '<leader>tn', ':tabn<CR>', opts)
-vim.keymap.set('n', '<leader>tp', ':tabp<CR>', opts)
+vim.keymap.set('n', '<leader>to', ':tabnew<CR>', { desc = '[O]pen new tab', noremap = true, silent = true })
+vim.keymap.set('n', '<leader>tc', ':tabclose<CR>', { desc = '[C]lose tab', noremap = true, silent = true })
+vim.keymap.set('n', '<leader>tn', ':tabn<CR>', { desc = '[N]ext tab', noremap = true, silent = true })
+vim.keymap.set('n', '<leader>tp', ':tabp<CR>', { desc = '[P]revious tab', noremap = true, silent = true })
 
 -- toggle line wrapping
-vim.keymap.set('n', '<leader>lm', '<cmd>set wrap!<CR>', opts)
+vim.keymap.set('n', '<leader>lm', '<cmd>set wrap!<CR>', { desc = 'Toggle line wrap', noremap = true, silent = true })
 
 -- Stay in indent mode while resizing
 vim.keymap.set('v', '<', '<gv', opts)
 vim.keymap.set('v', '>', '>gv', opts)
 
-vim.keymap.set('v', '<C-h>', '<gv', opts)
-vim.keymap.set('v', '<C-l>', '>gv', opts)
-
--- Keey last yanked when pasting
+-- Key last yanked when pasting
 vim.keymap.set('v', 'p', '"_dP', opts)
 
+-- navigation
+vim.keymap.set('n', 'B', '^', { noremap = false })
+vim.keymap.set('n', 'E', '$', { noremap = false })
+
 -- moving in insert mode.
-vim.keymap.set('i', '<C-b>', '<ESC>^i', { desc = 'move beginning of line' })
-vim.keymap.set('i', '<C-e>', '<End>', { desc = 'move end of line' })
-vim.keymap.set('i', '<C-h>', '<Left>', { desc = 'move left' })
-vim.keymap.set('i', '<C-l>', '<Right>', { desc = 'move right' })
-vim.keymap.set('i', '<C-j>', '<Down>', { desc = 'move down' })
-vim.keymap.set('i', '<C-k>', '<Up>', { desc = 'move up' })
+vim.keymap.set('i', '<C-b>', '<ESC>^i', { desc = 'Move beginning of line' })
+vim.keymap.set('i', '<C-e>', '<End>', { desc = 'Move end of line' })
+vim.keymap.set('i', '<C-h>', '<Left>', { desc = 'Move left' })
+vim.keymap.set('i', '<C-l>', '<Right>', { desc = 'Move right' })
+vim.keymap.set('i', '<C-j>', '<Down>', { desc = 'Move down' })
+vim.keymap.set('i', '<C-k>', '<Up>', { desc = 'Move up' })
 
 vim.keymap.set('n', '<Esc>', '<cmd>noh<CR>', { desc = 'general clear highlights' })
 
-vim.keymap.set({ 'n', 'v', 'i' }, '<C-s>', '<cmd>w<CR>', { desc = 'file save' })
-vim.keymap.set('n', '<leader>sn', '<Esc><cmd>noautocmd w<CR>', { desc = 'quit save' })
-vim.keymap.set('n', '<C-q>', '<cmd>q<CR>', { desc = 'quit save' })
-vim.keymap.set('n', '<C-c>f', '<cmd>%y+<CR>', { desc = 'file copy whole' })
+vim.keymap.set({ 'n', 'v', 'i' }, '<C-s>', '<cmd>w<CR>', { desc = 'Save file' })
+vim.keymap.set('n', '<leader>sn', '<Esc><cmd>noautocmd w<CR>', { desc = 'Esc and save' })
+vim.keymap.set('n', '<C-q>', '<cmd>q<CR>', { desc = 'Quit' })
+vim.keymap.set('n', '<C-c>f', '<cmd>%y+<CR>', { desc = 'Copy whole file' })
 
 -- global lsp mappings
 -- vim.keymap.set('n', '<leader>ds', vim.diagnostic.setloclist, { desc = 'lsp diagnostic loclist' })
@@ -110,38 +122,35 @@ vim.keymap.set('n', '<leader>d', vim.diagnostic.open_float, { desc = 'Open float
 vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostics list' })
 
 -- Comment
-vim.keymap.set('n', '<leader>/', 'gcc', { desc = 'comment toggle', remap = true })
-vim.keymap.set('v', '<leader>/', 'gc', { desc = 'comment toggle', remap = true })
+vim.keymap.set('n', '<leader>/', 'gcc', { desc = 'Comment toggle', remap = true })
+vim.keymap.set('v', '<leader>/', 'gc', { desc = 'Comment toggle', remap = true })
 
 -- telescope
-vim.keymap.set('n', '<leader>fw', '<cmd>Telescope live_grep<CR>', { desc = 'telescope live grep' })
-vim.keymap.set('n', '<leader>fb', '<cmd>Telescope buffers<CR>', { desc = 'telescope find buffers' })
-vim.keymap.set('n', '<leader>fh', '<cmd>Telescope help_tags<CR>', { desc = 'telescope help page' })
-vim.keymap.set('n', '<leader>ma', '<cmd>Telescope marks<CR>', { desc = 'telescope find marks' })
-vim.keymap.set('n', '<leader>fo', '<cmd>Telescope oldfiles<CR>', { desc = 'telescope find oldfiles' })
+vim.keymap.set('n', '<leader>fg', '<cmd>Telescope live_grep<CR>', { desc = 'Telescope live [g]rep' })
+vim.keymap.set('n', '<leader>fb', '<cmd>Telescope buffers<CR>', { desc = 'Telescope find [b]uffers' })
+vim.keymap.set('n', '<leader>fh', '<cmd>Telescope help_tags<CR>', { desc = 'Telescope [h]elp page' })
+vim.keymap.set('n', '<leader>ma', '<cmd>Telescope marks<CR>', { desc = 'Telescope find [m]arks' })
+vim.keymap.set('n', '<leader>fo', '<cmd>Telescope oldfiles<CR>', { desc = 'Telescope find [o]ldfiles' })
 vim.keymap.set('n', '<leader>fz', '<cmd>Telescope current_buffer_fuzzy_find<CR>',
-    { desc = 'telescope find in current buffer' })
-vim.keymap.set('n', '<leader>cm', '<cmd>Telescope git_commits<CR>', { desc = 'telescope git commits' })
-vim.keymap.set('n', '<leader>gt', '<cmd>Telescope git_status<CR>', { desc = 'telescope git status' })
-vim.keymap.set('n', '<leader>ff', '<cmd>Telescope find_files<cr>', { desc = 'telescope find files' })
+    { desc = 'Telescope find in current buffer[z]' })
+vim.keymap.set('n', '<leader>fc', '<cmd>Telescope git_commits<CR>', { desc = 'Telescope git [c]ommits' })
+vim.keymap.set('n', '<leader>fs', '<cmd>Telescope git_status<CR>', { desc = 'Telescope git [s]tatus' })
+vim.keymap.set('n', '<leader>ff', '<cmd>Telescope find_files<cr>', { desc = 'Telescope find [f]iles' })
 vim.keymap.set('n', '<leader>fa', '<cmd>Telescope find_files follow=true no_ignore=true hidden=true<CR>',
-    { desc = 'telescope find all files' })
+    { desc = 'Telescope find [a]ll files' })
 
 -- terminal
-vim.keymap.set('t', '<C-x>', '<C-\\><C-N>', { desc = 'terminal escape terminal mode' })
+-- <C-/> is mapped to opening the terminal, in a the snacks-nvim.lua file. 
+vim.keymap.set('t', '<C-x>', '<C-\\><C-N>', { desc = 'Terminal: escape terminal mode' })
 
 -- whichkey
-vim.keymap.set('n', '<leader>wK', '<cmd>WhichKey <CR>', { desc = 'whichkey all keymaps' })
+vim.keymap.set('n', '<leader>wK', '<cmd>WhichKey <CR>', { desc = 'Whichkey all keymaps' })
 
 -- Visual Maps
-vim.keymap.set('v', '<leader>r', '"hy:%s/<C-r>h//g<left><left>') -- find and replace
-vim.keymap.set('v', '<C-s>', ':sort<CR>')                        -- sort highlighted lines
-vim.keymap.set('v', 'J', ":m '>+1<CR>gv=gv")                     -- move current line up
-vim.keymap.set('v', 'K', ":m '>-2<CR>gv=gv")                     -- move current line down
-
--- navigation
-vim.keymap.set('n', 'B', '^', { noremap = false })
-vim.keymap.set('n', 'E', '$', { noremap = false })
+vim.keymap.set('v', '<leader>r', '"hy:%s/<C-r>h//g<left><left>', {desc = 'Find and replace'})
+vim.keymap.set('v', '<C-s>', ':sort<CR>', {desc = 'Sort highlighted lines'}) 
+vim.keymap.set('v', 'J', ":m '>+1<CR>gv=gv", {desc = 'Move current line up'})
+vim.keymap.set('v', 'K', ":m '>-2<CR>gv=gv" {desc = 'Move current line down'})
 
 -- vim.keymap.set('n', ':', '<cmd>FineCmdline<CR>', { noremap = true })
 
@@ -151,28 +160,12 @@ vim.keymap.set('n', 'H', ':PounceRepeat<CR>', { silent = true, desc = 'Pounce Re
 
 -- format
 -- defined in the lsp.lua file
--- vim.keymap.set('n', '<leader>=', function()
---     vim.lsp.buf.format()
--- end)
-vim.keymap.set('n', '==', 'ggVG', { silent = true, desc = 'Format buffer' })
-
--- lua
-vim.keymap.set('n', '<space>e', ':.lua<CR>', { silent = true, desc = 'Run lua on the highlighted lines.' })
-vim.keymap.set('v', '<space>e', ':lua<CR>', { silent = true, desc = 'Run lua on the highlighted lines.' })
-vim.keymap.set({ 'n', 'v' }, '<space>r', ':!lua %<CR>', { silent = true, desc = 'Run lua on entire file.' })
-
--- -- Map <Tab> to accept the completion suggestion
--- vim.keymap.set('i', '<Tab>', '<C-n>', { silent = false, desc = 'Accept completion suggestion' })
---
--- -- Map <C-n> to go to the next completion suggestion
--- vim.keymap.set('i', '<C-n>', '<C-n>', { silent = true, desc = 'Next completion suggestion' })
---
--- -- Map <CR> to insert the current suggestion
--- vim.keymap.set('i', '<CR>', '<C-y>', { silent = true, desc = 'Insert suggestion' })
---
--- -- Map <C-y> to insert the current suggestion
--- vim.keymap.set('i', '<C-y>', '<C-y>', { silent = true, desc = 'Insert suggestion' })
+vim.keymap.set('n', '<leader>=', function()
+    vim.lsp.buf.format()
+end, { silent = true, desc = 'Format buffer' })
 
 -- neogen
-vim.api.nvim_set_keymap('n', '<leader>nf', ":lua require('neogen').generate()<CR>", { noremap = true, silent = true })
-return {}
+vim.api.nvim_set_keymap('n', '<leader>nf', ":lua require('neogen').generate()<CR>", { desc = 'Generate documentation.', noremap = true, silent = true })
+
+-- zen-mode
+vim.keymap.set('n', '<leader><cr>', '<cmd>ZenMode<CR>', { desc = 'Toggle Zen Mode' })
